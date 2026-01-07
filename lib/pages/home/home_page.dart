2b45e554bc2c1db04d6aeb1b667c64a5a694758e
Rafael Santos <rafaelsantosfrr@gmail.com>
2026-01-07 14:24:49 -0300

    Initial commit - projeto Flutter

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gymapp/pages/home/widgets/workout_card.dart';
+import 'widgets/graficos/daytreinos.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,6 +11,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 19, 19, 19),
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 19, 19, 19),
         title: Image.asset('assets/images/nmlogo.png', height: 50),
@@ -31,11 +33,13 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start, // 👈 esquerda
           children: [
+            SizedBox(height: 60),
+
             Text(
               'Bem-vindo de volta',
               style: GoogleFonts.orbitron(
                 color: Color.fromARGB(255, 181, 181, 181),
-                fontSize: 16,
+                fontSize: 20,
                 fontWeight: FontWeight.normal,
               ),
             ),
@@ -43,14 +47,26 @@ class HomePage extends StatelessWidget {
             Text(
               'Rafael',
               style: GoogleFonts.orbitron(
-                fontSize: 22,
+                fontSize: 26,
                 color: Colors.deepOrange,
                 fontWeight: FontWeight.bold,
               ),
             ),
             SizedBox(height: 20),
-            
+
             WorkoutCard(),
+
+            SizedBox(height: 100),
+
+            Text(
+              'Seu progresso',
+              style: GoogleFonts.orbitron(
+                color: Colors.deepOrange,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            diasEmColuna(),
           ],
         ),
       ),
---
+++ lib/pages/home/widgets/graficos/daytreinos.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class diasEmColuna extends StatelessWidget {
+  const diasEmColuna({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(color: const Color.fromARGB(255, 152, 37, 37)),
+    );
+  }
+}
